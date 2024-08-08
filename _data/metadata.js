7fe1e35cae521feeaee40c91bb9bf091f1db1b11
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-08-08 12:07:51 -0700

    initial skeleton of a website up

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "43rd West Coast Conference on Formal Linguistics (WCCFL 43) @ UW",
+	url: "https://wccfl43.github.io/",
+	language: "en",
+	description: "Official website for the 43rd West Coast Conference on Formal Linguistics (WCCFL 43) at the University of Washington, Seattle, WA, USA. To be held April 25-27, 2025.",
+	author: {
+		name: "Shane Steinert-Threlkeld",
+		email: "shanest@uw.edu",
+		url: "https://www.shane.st/"
+	}
+}
---
+++ content/index.md
@@ -0,0 +1,11 @@
+---
+layout: layouts/base.njk
+title: 43rd West Coast Conference on Formal Linguistics
+eleventyNavigation:
+  key: Home
+  order: 0
+---
+
+# Welcome to WCCFL 43!
+
+Just testing out some stuff for now!
